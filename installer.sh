8d99749f8eb04655371239d5f516fc075c450534
maxdhctrl <duckhard.111@gmail.com>
2025-08-20 15:47:33 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,13 @@
 apt update -y
 
 apt install python3-pip -y
+apt install unzip -y
 
 pip3 install --upgrade tqdm
 pip3 install --upgrade psutil
 
+wget https://github.com/maxdhctrl/vt/releases/download/release/ffmpeg.zip
+unzip ffmpeg.zip
+
+chmod +x start.sh
+./start.sh
